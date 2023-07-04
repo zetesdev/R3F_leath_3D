7434d3ee457bceee1c8abc878dcfdc73e744513a
zetes_lenovo <zbigniew.sulkowski@gmail.com>
2023-07-04 14:48:04 +0200

    model element rotating test

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,4 +1,5 @@
 import { useGLTF } from '@react-three/drei';
+import { motion } from 'framer-motion-3d';
 
 export function Model(props) {
   const { progress } = props;
@@ -6,20 +7,29 @@ export function Model(props) {
 
   const customRotation = [0, 0, 0];
 
+  progress === 1 ? (customRotation[2] = -2) : (customRotation[2] = 0);
+
   return (
     <group {...props} dispose={null}>
-      <mesh
+      <motion.mesh
         geometry={nodes.element_02.geometry}
         material={materials.M_test_02}
         position={[-0.029977, 0.005329, 0.002927]}
-        rotation={customRotation}
+        animate={{
+          rotateZ: customRotation[2],
+        }}
+        transition={{
+          delay: 0.5,
+          duration: 1,
+          ease: 'easeInOut',
+        }}
       >
         <mesh
           geometry={nodes.element_03.geometry}
           material={materials.M_test_03}
           position={[0.0256305, 0.000169, -0.0023475]}
         />
-      </mesh>
+      </motion.mesh>
       <mesh
         geometry={nodes.element_01.geometry}
         material={materials.M_test_01}
