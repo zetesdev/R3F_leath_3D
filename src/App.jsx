0b9b10290eb834dc68975443ef15db92fe2b9b34
zetes_lenovo <zbigniew.sulkowski@gmail.com>
2023-07-06 14:06:55 +0200

    added onTouchMove direction recognition

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,48 @@ import { useState, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Experience from './components/Experience';
 import AnimTrigger from './components/AnimTrigger';
-import { Scroll, ScrollControls } from '@react-three/drei';
 import Scroller from './components/Scroller';
 
 function App() {
   const [progress, setProgress] = useState(0);
+  const touchValueY = useRef(0);
+  const touchMoveDown = useRef(null);
 
   const handleChangeProgress = (e) => {
-    if (e.type === 'wheel') {
-      // Obsługa eventu scrollowania myszką
+    switch (e.type) {
+      case 'wheel':
+        setProgress((prevProgress) => prevProgress + 1);
+        console.log('Mouse:', e.deltaY);
+        break;
 
-      setProgress((prevProgress) => prevProgress + 1);
-      console.log('Scrollowanie myszką:', e.deltaY);
-    } else if (e.type === 'touchmove') {
-      // Obsługa eventu dotyku
-      console.log('Przesunięcie dotyku:', e.touches[0].clientY);
+      case 'touchmove':
+        touchMoveDown.current = e.touches[0].clientY >= touchValueY.current;
+        touchValueY.current = e.touches[0].clientY;
+        console.log('tochMoveDown', touchMoveDown.current);
+        break;
+
+      case 'touchend':
+        console.log('FINAL:', touchValueY.current);
+        setProgress((prevProgress) => prevProgress + 1);
+        break;
+
+      default:
+        break;
     }
+  };
+
+  const debugLog = () => {
+    console.log(touchValueY.current);
   };
 
   return (
     <>
       <Scroller />
-      <Canvas onWheel={handleChangeProgress} onTouchMove={handleChangeProgress}>
+      <Canvas
+        onWheel={handleChangeProgress}
+        onTouchMove={handleChangeProgress}
+        onTouchEnd={handleChangeProgress}
+      >
         {/* <ScrollControls pages={1} damping={0.1}> */}
         <Experience progress={progress} />
 
@@ -48,6 +68,12 @@ function App() {
         </ScrollControls> */}
       </Canvas>
       <AnimTrigger progress={progress} changeProgress={handleChangeProgress} />
+      <button
+        onClick={debugLog}
+        className='z-23 fixed bottom-10 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-red-500 w-auto h-11 rounded-md'
+      >
+        DEBUG
+      </button>
     </>
   );
 }
