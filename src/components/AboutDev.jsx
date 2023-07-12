89c4c9b00865ff05d67576792e998e2b754dcfbe
zetes_lenovo <zbigniew.sulkowski@gmail.com>
2023-07-12 14:34:15 +0200

    some styling

--- src/components/AboutDev.jsx
+++ src/components/AboutDev.jsx
@@ -15,24 +15,24 @@ function About({ showAbout, setShowAbout }) {
     <>
       <button
         onClick={setShowAbout}
-        className='z-30 flex text-xl items-center justify-between text-white fixed top-0 right-0 bg-sky-500 w-32 h-auto lg:top-12 lg:right-12'
+        className='z-30 fixed top-0 right-0 flex text-2xl px-2 items-center justify-between text-white border-white border-2 bg-sky-500 w-32 h-auto lg:top-12 lg:right-12 lg:text-3xl lg:w-40 hover:bg-sky-700 transition-colors'
       >
-        <div className='mt-0'>{showAbout ? 'CLOSE' : 'ABOUT'}</div>
+        <div className='pb-0.5'>{showAbout ? 'CLOSE' : 'ABOUT'}</div>
         {/* <FaXmark />
         <FaBars /> */}
         {showAbout ? <FaXmark /> : <FaBars />}
         {/* <div
-          className={`bg-white h-0.5 rounded-md w-full transition-all ${
+          className={`bg-white h-0.5 rounded-lg w-full transition-all ${
             showAbout ? 'rotate-45  translate-y-0.5' : ''
           }`}
         />
         <div
-          className={`bg-white h-0.5 rounded-md w-full my-1 ${
+          className={`bg-white h-0.5 rounded-lg w-full my-1 ${
             showAbout ? 'hidden' : ''
           }`}
         />
         <div
-          className={`bg-white h-0.5 rounded-md w-full transition-all ${
+          className={`bg-white h-0.5 rounded-lg w-full transition-all ${
             showAbout ? '-rotate-45' : ''
           }`}
         /> */}
@@ -44,16 +44,16 @@ function About({ showAbout, setShowAbout }) {
           showAbout ? 'w-full' : 'w-0'
         }`}
       >
-        <div className='flex flex-col items-center p-6 m-3 space-y-10 bg-white shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16'>
+        <div className='flex flex-col items-center p-6 m-3 space-y-10  bg-white shadow-2xl lg:flex-row lg:space-y-0 lg:space-x-10 lg:m-0 lg:p-16'>
           {/* <!-- Label & Title Container --> */}
-          <div className='flex flex-col mb-4 space-y-3 text-center md:text-left'>
+          <div className='flex flex-col mb-4 space-y-3 text-center lg:text-md lg:text-left'>
             {/* <!-- Title --> */}
             <p>&#x1F44B; hello, i am Zbigniew</p>
-            <h1 className='max-w-sm text-3xl font-medium pb-5'>
+            <h1 className='max-w-sm text-2xl font-medium pb-5 lg:text-3xl'>
               I work as <span className='text-red-500'> frontend </span> dev and{' '}
               <span className='text-red-500'>3D </span>technical generalist.
             </h1>
-            <p className='text-sm max-w-sm font-light'>
+            <p className='text-sm max-w-sm font-light lg:text-m'>
               This site is my R&D project, where i tried to explore connection
               between modern web development stack and 3D art world.
             </p>
@@ -62,7 +62,7 @@ function About({ showAbout, setShowAbout }) {
               You can see more here:
             </p>
             {/* LINKS CONTAINER */}
-            <div className='flex flex-col space-y-4 md:space-y-0 md:space-x-4 md:flex-row'>
+            <div className='flex flex-col space-y-4 lg:space-y-0 lg:space-x-4 lg:flex-row'>
               <a
                 href={BEHANCE_URL}
                 className='flex items-center justify-center py-3 px-5 space-x-3 border-2 border-gray-300 shadow-sm  hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150'
@@ -81,7 +81,7 @@ function About({ showAbout, setShowAbout }) {
             </div>
           </div>
 
-          <div className='w-32 md:w-60'>
+          <div className='w-32 lg:w-60'>
             <img src={facePhoto} alt='' className='rounded-full' />
           </div>
         </div>
